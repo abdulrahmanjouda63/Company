744076bedb99ce66fd482e29835ef3154395687f
abdulrahmanjouda63 <100361517+abdulrahmanjouda63@users.noreply.github.com>
2025-03-09 15:57:12 +0200

    Create Create (Action - View)

---
+++ Company.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code is required")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "CreateAt is required")]
+        public DateTime CreateAt { get; set; }
+    }
+}
