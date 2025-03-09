b8184a748c619884ccade7588d0b71909fb66dae
abdulrahmanjouda63 <100361517+abdulrahmanjouda63@users.noreply.github.com>
2025-03-09 14:00:00 +0200

    Create Department Controller And Allow DI For All Services

---
+++ Company.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,26 @@
+﻿using Company.BLL.Interfaces;
+using Company.BLL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PL.Controllers
+{
+    // MVC Controller
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        // ASK CLR Create Object From DepartmentRepository
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+        [HttpGet] //GET: Department/Index
+        public IActionResult Index()
+        {
+
+            var departments = _departmentRepository.GetAll();
+
+            return View(departments);
+        }
+    }
+}
