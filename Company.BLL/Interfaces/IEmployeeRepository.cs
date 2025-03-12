ef6f65aaee5a125bd5dc602a7adc6a8b556cc2b5
abdulrahmanjouda63 <100361517+abdulrahmanjouda63@users.noreply.github.com>
2025-03-12 21:43:17 +0200

    Refactoring To Use Generic Repository

--- Company.BLL/Interfaces/IEmployeeRepository.cs
+++ Company.BLL/Interfaces/IEmployeeRepository.cs
@@ -7,14 +7,14 @@ using Company.DAL.Models;
 
 namespace Company.BLL.Interfaces
 {
-    public interface IEmployeeRepository
+    public interface IEmployeeRepository : IGenericRepository<Employee>
     {
-        IEnumerable<Employee> GetAll();
+        //IEnumerable<Employee> GetAll();
 
-        Employee? Get(int id);
+        //Employee? Get(int id);
 
-        int Add(Employee model);
-        int Update(Employee model);
-        int Delete(Employee model);
+        //int Add(Employee model);
+        //int Update(Employee model);
+        //int Delete(Employee model);
     }
 }
