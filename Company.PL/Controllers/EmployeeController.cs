b27fa6f1c17b04f1af0c1d71f79d8dc56ed0202f
abdulrahmanjouda63 <100361517+abdulrahmanjouda63@users.noreply.github.com>
2025-03-12 23:45:47 +0200

    Create Employee Service

---
+++ Company.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,153 @@
+﻿using Company.BLL.Interfaces;
+using Company.DAL.Models;
+using Company.PL.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            var employees = _employeeRepository.GetAll();
+
+            return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateEmployeeDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee()
+                {
+                    Name = model.Name,
+                    Address = model.Address,
+                    Age = model.Age,
+                    Email = model.Email,
+                    Phone = model.Phone,
+                    Salary = model.Salary,
+                    IsActive = model.IsActive,
+                    IsDeleted = model.IsDeleted,
+                    HiringDate = model.HiringDate,
+                    CreateAt = model.CreateAt,
+                };
+                var Count = _employeeRepository.Add(employee);
+                if (Count > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Details(int? id, string ViewName = "Details")
+        {
+            if (id == null)
+                return NotFound();
+
+            var Employee = _employeeRepository.Get(id.Value);
+            if (Employee == null)
+                return NotFound();
+
+            return View(Employee);
+        }
+
+        [HttpGet]
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var employee = _employeeRepository.Get(id.Value);
+            if (employee == null)
+                return NotFound();
+
+            var model = new CreateEmployeeDto
+            {
+                Name = employee.Name,
+                Address = employee.Address,
+                Age = employee.Age,
+                Email = employee.Email,
+                Phone = employee.Phone,
+                Salary = employee.Salary,
+                IsActive = employee.IsActive,
+                IsDeleted = employee.IsDeleted,
+                HiringDate = employee.HiringDate,
+                CreateAt = employee.CreateAt,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, CreateEmployeeDto  model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee()
+                {
+                    Id = id,
+                    Name = model.Name,
+                    Address = model.Address,
+                    Age = model.Age,
+                    Email = model.Email,
+                    Phone = model.Phone,
+                    Salary = model.Salary,
+                    IsActive = model.IsActive,
+                    IsDeleted = model.IsDeleted,
+                    HiringDate = model.HiringDate,
+                    CreateAt = model.CreateAt,
+                };
+                var Count = _employeeRepository.Update(employee);
+                if (Count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            return Details(id, "Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int? id, Employee employee)
+        {
+            if (id != employee.Id)
+                return BadRequest();
+
+            try
+            {
+                _employeeRepository.Delete(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(employee);
+            }
+
+        }
+    }
+}
